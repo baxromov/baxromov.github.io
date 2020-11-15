e17d908b6159af1d1da89d7babd8ca3ae80d37fc
baxromov <baxromov.shahzodbek@gmail.com>
2020-11-15 17:19:08 +0500

    Add news section

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@
     $('.prev').click(function(){ $('.carousel').carousel('prev');return false; });
     
 })(jQuery);
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        loop:true,
+        items:3,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[600,1],
+        pagination:false,
+        navigationText:false,
+        autoPlay:true
+    });
+});
